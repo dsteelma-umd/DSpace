baf2343bc356a1da95ecdde76b3b9a4d3beac3ec
David P. Steelman <dsteelma@umd.edu>
2022-08-04 10:43:38 -0400

    LIBDRUM-657. WIP for CAS authentication
    See https://confluence.umd.edu/display/~dsteelma/Jira+-+LIBDRUM-657
    for discussion.
    
    This WIP basically only shows that successful CAS authentication is
    possible.
    
    Also, the "minimal" set of changes needed to make CAS authentication
    work has not been determined, and are probably not anywhere near the
    changes needed for a "final" implementation. For example, ideally the
    changes in the "CASLoginFilter" and "WebSecurityConfiguration" files
    should be moved to the "additions" modules, and not be in DSpace core.
    
    https://issues.umd.edu/browse/LIBDRUM-657

--- dspace-server-webapp/src/main/java/org/dspace/app/rest/security/WebSecurityConfiguration.java
+++ dspace-server-webapp/src/main/java/org/dspace/app/rest/security/WebSecurityConfiguration.java
@@ -137,6 +137,11 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
             .addFilterBefore(new ShibbolethLoginFilter("/api/authn/shibboleth", authenticationManager(),
                                                        restAuthenticationService),
                              LogoutFilter.class)
+            .addFilterBefore(new CASLoginFilter("/api/authn/cas", authenticationManager(),
+                                                restAuthenticationService),
+                             LogoutFilter.class
+                          )
+
             //Add a filter before our OIDC endpoints to do the authentication based on the data in the
             // HTTP request
             .addFilterBefore(new OidcLoginFilter("/api/authn/oidc", authenticationManager(),
