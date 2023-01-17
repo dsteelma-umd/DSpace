c9d451133d594ad66da3a656443efae6111d10db
David P. Steelman <dsteelma@umd.edu>
2023-01-17 11:55:10 -0500

    LIBDRUM-711. Initial implementation of "Equitable Access" field display
    "Equitable Access" field is displayed, but no action is taken based on
    value.
    
    https://umd-dit.atlassian.net/browse/LIBDRUM-711

---
+++ dspace/modules/server/src/main/java/org/dspace/app/rest/submit/step/EquitableAccessStep.java
@@ -0,0 +1,9 @@
+package org.dspace.app.rest.submit.step;
+
+import org.dspace.app.util.DCInputsReaderException;
+
+public class EquitableAccessStep extends DescribeStep {
+    public EquitableAccessStep() throws DCInputsReaderException {
+        super();
+    }
+}
